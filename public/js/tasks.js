7a23728b6dde2e9e293dc6833ce12ab464c0f834
Pontus Pettersson <pontusp66j@gmail.com>
2016-09-01 14:54:27 +0000

    test

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -153,6 +153,7 @@ function loadItems() {
 			console.log(resource);
 			if (resource == -1)
 				usedResource += parseFloat(project.substring(resource).replace('(', '').replace(')', ''));
+				console.log(usedResource);
 			$("#existing_project").append('<option value="' + project + '">' + project + '</option>');
 			var projectString = '<div style="width: ' + Math.round(projectWidth) + 'px;" class="project" data-project="' + project + '"><div class="name ' + (resource !== -1 ? 'has-resources' : '') + '">' + project.replace('(', '').replace(')', '%') + ' <span class="add">+</span></div><div class="item-list">';
 			$.each(items, function (order, item) {
